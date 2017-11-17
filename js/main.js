8c703a5643326a0688f97fa0e7549866927a4b7b
Leila.L <leila.lababsa@hotmail.fr>
2017-11-17 14:35:51 +0100

    work on function js for card reveal

--- js/main.js
+++ js/main.js
@@ -30,35 +30,10 @@ function before() {
 
 
 // ************ CARDS ***************
-// var click = document.getElementsByClassName("screen");
-//
-// function cardReveal(img) {
-// img.style.opacity = 0;
-// var carte = img.parentNode;
-//   carte.append("<div><h4>??</h4><br><br><p>???</p></div>");
-// }
-
-// $(".info").click(function() {
-//   cardReveal();
-// })
-//
-// function cardReveal() {
-//   $(".cardImage").append("<div><h4>Reproduction page d'accueuil LeBonCoin</h4><br><br><p>Le site est responsive et mobile first. Avec un effet cliquable sur la map. Utilisation de bootstrap.</p></div>");
-// }
-
-//
-// function masquer_div(a_masquer)
-// {
-//   if (document.getElementById("a_masquer").style.display == 'none') {
-//        document.getElementById("a_masquer").style.display = 'block';
-//   }
-//   else {
-//        document.getElementById("a_masquer").style.display = 'none';
-//   }
-// }
 
 $(".info").mouseenter(function(){
   $(this).siblings(".a_masquer").css("display","block");
+  // $(".a_masquer").css("display","block");
 });
 $(".info").mouseleave(function(){
   $(this).siblings(".a_masquer").css("display","none");
