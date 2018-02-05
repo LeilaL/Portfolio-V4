4ab5ce1e3fe774a4429c99a10b1ef63f823f4630
Leila.L <leila.lababsa@hotmail.fr>
2018-02-05 18:09:04 +0100

    fixed js problem; show div on card portfolio when hover + changed the position of cards + added new cv & img

--- js/main.js
+++ js/main.js
@@ -1,82 +1,37 @@
-// ************ SLIDER ***************
-// var index = 0;
-// var carousel = document.getElementsByClassName("images");
-//
-// setInterval(before,2700);
-//
-// function slider () {
-// carousel[index].style.display = -1;
-// if (index==3){
-//   index=0;
-//   carousel[index].style.zIndex = 1;
-// }
-// else {
-//   index++;
-//   carousel[index].style.zIndex = 1;
-// }
-// }
-//
-// function before() {
-//   carousel[index].style.zIndex = -1;
-//   if (index==0){
-//     index=3;
-//     carousel[index].style.zIndex = 1;
-//   }
-//   else {
-//     index--;
-//     carousel[index].style.zIndex = 1;
-//   }
-// }
-
-
 // ************ CARDS REVEAL ***************
 
-// $(".info").click(function(){
-//   if (window.matchMedia("(max-width: 992px)").matches) {
-//       /* The viewport is less than, or equal to, 992 pixels wide */
-//         alert("test smartphone");
-//         if($(".a_masquer").css("display") == "none"){
-//           $(this).siblings(".a_masquer").css("display","block");
-//         }
-//         else{
-//           $(this).siblings(".a_masquer").css("display","none");
-//         }
-//   }
-// });
+var clicked = false;
+
+$(".info").click(function(){
+  if (window.matchMedia("(max-width: 992px)").matches) {
+            if($(this).siblings(".a_masquer").css("display") == "none"){
+              $(this).siblings(".a_masquer").css("display","block");
+            }
+            else{
+              $(this).siblings(".a_masquer").css("display","none");
+            }
+  }
+});
+
+  if (window.matchMedia("(min-width: 993px)").matches) {
+
+    var onAmasquer = false;
 
 
     $(".info").mouseenter(function(){
       $(this).siblings(".a_masquer").css("display","block");
     });
 
-    $(".info").mouseleave(function(){
-      $(".screen").mouseleave(function(){
-        $(this).siblings(".a_masquer").css("display","none");
-      });
+    $(".screen").mouseleave(function(){
+
+          $(".a_masquer").mouseleave(function(){
+            $(this).css("display","none");
+          });
+
+        $(".a_masquer").mouseenter(function(){
+          $(this).css("display","block");
+        });
+
+
     });
-
-    $(".a_masquer").mouseenter(function(){
-      $(".a_masquer").mouseleave(function(){
-        $(this).css("display","none");
-      });
-    });
-
-
-// if (screen.width < 768) {
-//   $("#lien1").attr("data-toggle","collapse");
-//   $("#lien1").attr("data-target",".navbar-collapse");
-//   $("#lien2").attr("data-toggle","collapse");
-//   $("#lien2").attr("data-target",".navbar-collapse");
-//   $("#lien3").attr("data-toggle","collapse");
-//   $("#lien3").attr("data-target",".navbar-collapse");
-// }
-
-
-// $(".info").hover(function(){
-//     $(this).siblings(".screen").css("opacity", 1);
-//     $(this).siblings(".screen").css("-webkit-filter", "none");
-//     }
-//
-// $(".info").mouseleave(function(){
-//       $(this).siblings(".screen").css("filter", "grayscale(100%)");
-// });
+  }
